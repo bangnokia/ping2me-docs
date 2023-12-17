fc531217c5ac6c713e06b1b1025ec31b7f829e80
bangnokia <bangnokia@gmail.com>
2023-12-18 02:42:44 +0700

    add GA

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -40,7 +40,21 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/bangnokia/ping2me-docs' },
     ]
-  }
+  },
+  head: [
+    [
+      'script',
+      { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-LD38LCJWNB' }
+    ],
+    [
+      'script',
+      {},
+      `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-LD38LCJWNB');`
+    ]
+  ]
 })
 
 
